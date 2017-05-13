db73aaf78105209a58b882441a6423493a70a4e6
Brittany Baranowski <brittanybaranowski@gmail.com>
2017-05-13 07:57:56 -0500

    Create reader and writer password methods, and add authentication method with some validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,27 @@ class User < ApplicationRecord
   # Remember to create a migration!
   has_many :rounds
   has_many :guesses, through: :rounds
+
+  validates :email, presence: true, uniqueness: true
 end
+
+def password
+  @password = BCrypt::Password.new(encrypted_password)
+end
+
+def password=(new_password)
+  @password = BCrypt::Password.create(new_password)
+  self.encrypted_password = @password
+end
+
+self.authenticate(user = {})
+  @user = self.find_by(email: params[:email])
+
+  if @user.password == params[:password]
+    @user
+  else
+    nil
+  end
+end
+
+
