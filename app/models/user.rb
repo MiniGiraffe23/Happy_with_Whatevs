429c5b7a56fc3bbc8496c41e5f2e6b2ca0e10e84
Brittany Baranowski & Zach Parsons & Braidy Merkle <pair+MiniGiraffe23.ZachParsons.bmerkle3@devbootcamp.com>
2017-05-12 19:00:14 -0500

    Create login, authentication method, post and get routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,26 @@ class User < ApplicationRecord
   # Remember to create a migration!
   has_many :rounds
   has_many :guesses, through: :rounds
+
+  validates :email, presence: true, uniqueness: true
+  validates :username, null: false
+
+  def password
+    @password ||= BCrypt::Password.new(encrypted_password)
+  end
+
+  def password=(new_password)
+    @password = BCrypt::Password.create(new_password)
+    self.encrypted_password = @password
+  end
+
+  def self.authenticate(params = {})
+    @user = self.find_by(email: params[:email])
+
+    if @user.password == params[:password]
+      @user
+    else
+      nil
+    end
+  end
 end
