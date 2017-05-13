bb6a234adb4f5a7352f53df92075bc4efcfd29ff
Brittany Baranowski <brittanybaranowski@gmail.com>
2017-05-13 09:07:47 -0500

    Copy over some code from work done with pairs, create new view folders/files and contorllers

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,2 +1,20 @@
 get '/' do
+  redirect '/decks'
+end
+
+get '/login' do
+  @user = User.new
+  erb :'/login'
+end
+
+post '/login' do
+  p params
+  @user = User.authenticate(params[:user])
+  p params
+  if @user
+    session[:user_id] = @user.id
+    redirect '/decks'
+  else
+    redirect '/login'
+  end
 end
