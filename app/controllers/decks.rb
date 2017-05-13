27db70a9cf4af5a95dc1bfbbc0fbda2a5c35e713
Joshua Britton & Daniel Wickman <pair+brittonjoshua.dwwickman@devbootcamp.com>
2017-05-13 15:19:33 -0500

    loop forever

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,27 +1,33 @@
 get '/decks' do
   @decks = Deck.order(:topic)
+  @cards = Card.new
   erb :'/decks/index'
 end
 
-get '/decks/:deck_id/cards/:card_id' do
+get '/decks/:deck_id/cards/next' do
+  p params
   @deck = Deck.find(params[:deck_id])
-  @card = @deck.cards.find(params[:card_id])
+  @card = @deck.cards.all.sample
   erb :'cards/show'
 end
 
+# get '/decks/:deck_id/cards/' do
+#   redirect "/decks/#{deck.id}/cards/#{card.id}"
+# end
 
 #if answer is true.  Update Guess.correct to true
 #remove card from deck
 #show answer is correct onpage button to go tonext question
 
 #if wrong display wrong.  put card in back. display correc submit button question
-post '/decks/:deck_id/cards/:card_id' do
-  @deck = Deck.find(params([:deck_id]))
-  @card = deck.cards.find(params[:card_id])
+put '/decks/:deck_id/cards/:id' do
+  @deck = Deck.find(params[:deck_id])
+  @card = @deck.cards(params[:id])
   if params[:user_guess] == @card.answer
-    card.guess.correct = true
-
+    @card.guess.correct = true
   else
-    thigns are false
+    p "were in else loop"
   end
+  redirect "/decks/#{@deck.id}/cards/next"
 end
+
