86f8f69f3ed7907475ed57a92d7a66529ca881b8
Joshua Britton & Daniel Wickman <pair+brittonjoshua.dwwickman@devbootcamp.com>
2017-05-13 11:41:07 -0500

    worked on deck

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -1,3 +1,27 @@
 get '/decks' do
+  @decks = Deck.order(:topic)
   erb :'/decks/index'
+end
+
+get '/decks/:deck_id/cards/:card_id' do
+  @deck = Deck.find(params[:deck_id])
+  @card = @deck.cards.find(params[:card_id])
+  erb :'cards/show'
+end
+
+
+#if answer is true.  Update Guess.correct to true
+#remove card from deck
+#show answer is correct onpage button to go tonext question
+
+#if wrong display wrong.  put card in back. display correc submit button question
+post '/decks/:deck_id/cards/:card_id' do
+  @deck = Deck.find(params([:deck_id]))
+  @card = deck.cards.find(params[:card_id])
+  if params[:user_guess] == @card.answer
+    card.guess.correct = true
+
+  else
+    thigns are false
+  end
 end
