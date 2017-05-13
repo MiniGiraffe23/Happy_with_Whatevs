5080ee6d0d8bdaab46568937c2bdf08a26ffdc68
Joshua Britton & Daniel Wickman <pair+brittonjoshua.dwwickman@devbootcamp.com>
2017-05-13 15:57:08 -0500

    Working on comparing answer with correct answer

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -22,11 +22,12 @@ end
 #if wrong display wrong.  put card in back. display correc submit button question
 put '/decks/:deck_id/cards/:id' do
   @deck = Deck.find(params[:deck_id])
-  @card = @deck.cards(params[:id])
+  @card = @deck.cards.find(params[:id])
+ @card
   if params[:user_guess] == @card.answer
-    @card.guess.correct = true
-  else
-    p "were in else loop"
+    @card.guesses.update_attributes(params[correct: true])
+  # else
+  #   @card.guesses.update_attributes(params[:correct])
   end
   redirect "/decks/#{@deck.id}/cards/next"
 end
